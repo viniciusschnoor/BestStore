da11e39cfee0039fbeb89f0b7c5d14be6f4e93de
Vinicius Schnoor <vschnooor@outlook.com>
2024-11-28 21:38:24 -0300

    Create the Product Model

---
+++ BestStoreMVC/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace BestStoreMVC.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [MaxLength(100)]
+        public string Brand { get; set; } = string.Empty;
+
+        [MaxLength(100)]
+        public string Category {get; set; } = string.Empty;
+
+        [Precision(16, 2)]
+        public decimal Price { get; set; }
+        public string Description { get; set; } = string.Empty;
+
+        [MaxLength(100)]
+        public string ImageFileName { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+    }
+}
--- BestStoreMVC/obj/Debug/net8.0/BestStoreMVC.AssemblyInfo.cs
+++ BestStoreMVC/obj/Debug/net8.0/BestStoreMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BestStoreMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddbb1cab2584bbe305bc49c0432fa918947f78af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfeb75f97e50bf27d780c478087c77a0a22819b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("BestStoreMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BestStoreMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
