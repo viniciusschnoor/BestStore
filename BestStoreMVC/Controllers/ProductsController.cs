752b964299b2a2ee4c0703f00192e1572ae80d34
Vinicius Schnoor <vschnooor@outlook.com>
2024-12-24 19:39:06 -0300

    Update Products - Save new Details
    Now it's possible save all modifications

--- BestStoreMVC/Controllers/ProductsController.cs
+++ BestStoreMVC/Controllers/ProductsController.cs
@@ -43,6 +43,7 @@ namespace BestStoreMVC.Controllers
             DateTime CreateDateTime = DateTime.Now;
             string newFileName = CreateDateTime.ToString("yyyyMMddHHmmssfff");
             newFileName += Path.GetExtension(productDto.ImageFile!.FileName);
+
             string imageFullPath = environment.WebRootPath + "/products/" + newFileName;
             using (FileStream stream = System.IO.File.Create(imageFullPath))
             {
@@ -91,6 +92,54 @@ namespace BestStoreMVC.Controllers
             ViewData["CreatedAt"] = product.CreatedAt;
 
             return View(productDto);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, ProductDto productDto)
+        {
+            var product = context.Products.Find(id);
+
+            if (product == null)
+            {
+                return RedirectToAction("Index", "Products");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                ViewData["ProductId"] = product.Id;
+                ViewData["ImageFileName"] = product.ImageFileName;
+                ViewData["CreatedAt"] = product.CreatedAt;
+
+                return View(productDto);
+            }
+
+            // Update the Image File if we have a new
+            string newFileName = product.ImageFileName;
+            if (productDto.ImageFile != null)
+            {
+                newFileName = DateTime.Now.ToString("yyyyMMddHHmmssfff");
+                newFileName += Path.GetExtension(productDto.ImageFile!.FileName);
+
+                string imageFullPath = environment.WebRootPath + "/products/" + newFileName;
+                using (var stream = System.IO.File.Create(imageFullPath))
+                {
+                    productDto.ImageFile.CopyTo(stream);
+                }
+
+                // Delete the old image
+                string oldImageFullPath = environment.WebRootPath + "/products/" + product.ImageFileName;
+                System.IO.File.Delete(oldImageFullPath);
+            }
+
+            product.Name = productDto.Name;
+            product.Brand = productDto.Brand;
+            product.Category = productDto.Category;
+            product.Price = productDto.Price;
+            product.Description = productDto.Description;
+            product.ImageFileName = newFileName;
+
+            context.SaveChanges();
+            return RedirectToAction("Index", "Products");
         }
     }
 }
--- BestStoreMVC/obj/Debug/net8.0/BestStoreMVC.AssemblyInfo.cs
+++ BestStoreMVC/obj/Debug/net8.0/BestStoreMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BestStoreMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7f0bda12acfacec7090c571bde5da27c5c00158")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a2a995bfaabf3e613d8755381068130dea9f1dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("BestStoreMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BestStoreMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
