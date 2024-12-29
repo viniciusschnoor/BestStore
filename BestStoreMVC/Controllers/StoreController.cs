8b766122ed3d5678f8a689aad9e6f0362b7fca0f
Vinicius Schnoor <vschnooor@outlook.com>
2024-12-29 20:58:32 -0300

    StoreController - Add Search Sort and Filter Functionalities

--- BestStoreMVC/Controllers/StoreController.cs
+++ BestStoreMVC/Controllers/StoreController.cs
@@ -13,15 +13,46 @@ namespace BestStoreMVC.Controllers
         {
             this.context = context;
         }
-        public IActionResult Index(int pageIndex)
+
+        public IActionResult Index(int pageIndex, string? search, string? brand, string? category, string? sort)
         {
             //var products = context.Products.OrderByDescending(p => p.Id).ToList();
 
             // Create the Query (context.Products)
             IQueryable<Product> query = context.Products;
 
+            // Search Functionality
+            if (search != null && search.Length > 0)
+            {
+                query = query.Where(p => p.Name.Contains(search));
+            }
+
+            // Filter Functionality
+            if (brand != null && brand.Length > 0)
+            {
+                query = query.Where(p => p.Brand.Contains(brand));
+            }
+
+            if (category != null && category.Length > 0)
+            {
+                query = query.Where(p => p.Category.Contains(category));
+            }
+
+            // Sort Functionality
+            if (sort == "price_asc")
+            {
+                query = query.OrderBy(p => p.Price);
+            } else if (sort == "price_desc")
+            {
+                query = query.OrderByDescending(p => p.Price);
+            } else
+            {
+                // Newest First
+                query = query.OrderByDescending(p => p.Id);
+            }
+
             // Update the Query to Sort Products (.OrderByDescending(p => p.Id))
-            query = query.OrderByDescending(p => p.Id);
+            // query = query.OrderByDescending(p => p.Id);  -->  Sort Functionatility
 
             // Implementation of Pagination Functionality
             if (pageIndex < 1)
@@ -41,7 +72,15 @@ namespace BestStoreMVC.Controllers
             ViewBag.PageIndex = pageIndex;
             ViewBag.TotalPages = totalPages;
 
-            return View(products);
+            var storeSearchModel = new StoreSearchModel()
+            {
+                Search = search,
+                Brand = brand,
+                Category = category,
+                Sort = sort
+            };
+
+            return View(storeSearchModel);
         }
     }
 }
